5dbe621e8fb5302d94a47f958596e823cc92300c
Cadu <cadumalmeida13@gmail.com>
2025-09-14 17:22:25 -0300

    aprendendo tipo em java

---
+++ variaveis.java
@@ -0,0 +1,16 @@
+import java.sql.SQLOutput;
+
+public class variaveis {
+    public static void main(String[] args) {
+        // o nome do tipo de variavel sempre começa com maiusculo
+        String nome = "cadu";
+        int idade = 19;
+        double nota = 7.3;
+        float nota2 = 9.4f;
+        // sempre que declarar um float deve botar um "f" no final do valor.
+        System.out.println("Nome: " + nome);
+        System.out.println("idade: " + idade);
+        System.out.println("nota media: " + nota);
+        System.out.println("nota media 2: " + nota2);
+    }
+}
