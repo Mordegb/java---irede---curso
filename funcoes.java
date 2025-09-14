ffda9f543056486defa6a104c821a5f0c2997e48
Cadu <cadumalmeida13@gmail.com>
2025-09-14 20:01:07 -0300

    funções

--- funcoes.java
+++ funcoes.java
@@ -4,17 +4,19 @@ public class funcoes {
         System.out.println("ola a todos");
     }
 
-    // função
+    // função com parametro
     void apresentaNome(String nome){
         System.out.println("bem vindo " + nome);
     }
-
+    //função com retorno
     int quadrado(int num){ //parametro com retorno
         num = num * num;
         return num;
     }
 
     public static void main(String[] args){
+        // ao usar funções deve sempre usar "new" seguido do nome do arquivo
+        //e um "." com o nome da função
         new funcoes().apresenta();
         new funcoes().apresentaNome("cadu");
         int num = new funcoes().quadrado(2);
