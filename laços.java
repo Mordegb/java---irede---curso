881c6f80c7941ec63a8fc73b32889bfe52882c64
Cadu <cadumalmeida13@gmail.com>
2025-09-15 14:58:10 -0300

    como usar laços em java.

---
+++ laços.java
@@ -0,0 +1,27 @@
+
+public class laços {
+    public static void main(String[] args) {
+        //laço em java é mto igual ao C mds
+        for(int i = 0; i < 10; i++){
+            System.out.println(i + 1);
+        }
+        System.out.println("\n");
+        //laço em for
+
+        int i = 0;
+        while(i < 10){
+            System.out.println(i + 1);
+            i++;
+        }
+        System.out.println("\n");
+        //laço em while
+
+
+        int j = 0;
+        do {
+            System.out.println(j + 1);
+            j++;
+        }while(j < 10);
+        //laço em do while
+    }
+}
