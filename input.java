b29beef515af6698f109495121c4710dee671d69
Cadu <cadumalmeida13@gmail.com>
2025-09-15 13:49:34 -0300

    fazendo input no java

--- input.java
+++ input.java
@@ -1,5 +1,18 @@
+import java.util.Scanner;
+//pra fazer input no java precisa importar essa biblioteca
+
 public class input {
     public static void main(String[] args) {
-        //pesquisar como fazer input no java
+        // o "abaixo" acima é o comando que a gente usa pra inserir variaveis
+        Scanner sc = new Scanner(System.in);
+
+        String nome = sc.nextLine(); //pode trocar o "sc pelo oque eu quiser."
+        int idade = sc.nextInt(); // após usar o next,eu completo com o tipo de variavel com primeira letra maiuscula
+        double nota = sc.nextDouble();
+
+        System.out.println("nome: " + nome);
+        System.out.println("idade: " + idade);
+        System.out.println("nota: " + nota);
+
     }
 }
