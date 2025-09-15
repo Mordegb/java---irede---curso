0dc10deab80ce8906057d3edca0b980181cb5613
Cadu <cadumalmeida13@gmail.com>
2025-09-15 19:28:00 -0300

    vetores e matrizes em java

---
+++ arrays.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class arrays {
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        // declarar um vertor
+        int[] valores = new int[5]; // declara o array e seu tamanho
+
+        for (int i = 0; i < valores.length; i++){ // o comando "valores.length"
+            valores[i] = input.nextInt();
+
+        }
+
+        for (int i = 0; i < valores.length; i++){ // o comando "valores.length"
+            valores[i] = (valores [i] * 2);
+            System.out.println((valores [i] * 2));
+
+        }
+
+        //declarar uma matriz
+        int[][] matriz = new int[3][3]; //declarando uma matriz
+
+        for (int i = 0; i < matriz.length; i++){
+            for (int j = 0; j < matriz[i].length; j++){
+                matriz[i][j] = (i + j);
+                System.out.println(matriz[i][j]);
+
+
+            }
+        }
+
+    }
+}
