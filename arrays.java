ad3b01aab7035575ec3c3d40bf2980fab52520bb
Cadu <cadumalmeida13@gmail.com>
2025-09-16 09:40:49 -0300

    exemplos de arrays e matriz

--- arrays.java
+++ arrays.java
@@ -1,24 +1,26 @@
 import java.util.Scanner;
 
 public class arrays {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         // declarar um vertor
         int[] valores = new int[5]; // declara o array e seu tamanho
 
-        for (int i = 0; i < valores.length; i++){ // o comando "valores.length"
+        Integer[] valores2 = {1, 2, 3, 4, 5}; //tmb da pra criar direto.
+
+        for (int i = 0; i < valores.length; i++) { // o comando "valores.length"
             valores[i] = input.nextInt();
-
+            valores[i] = (valores[i] * 2);
         }
 
-        for (int i = 0; i < valores.length; i++){ // o comando "valores.length"
-            valores[i] = (valores [i] * 2);
-            System.out.println((valores [i] * 2));
-
+        for (int i = 0; i < valores2.length; i++) {
+            System.out.println(valores[i]);
         }
+
+            System.out.println("\n");
 
         //declarar uma matriz
-        int[][] matriz = new int[3][3]; //declarando uma matriz
+       int[][] matriz = new int[3][3]; //declarando uma matriz
 
         for (int i = 0; i < matriz.length; i++){
             for (int j = 0; j < matriz[i].length; j++){
