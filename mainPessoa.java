7ea2508c2872296d3f0a134d5944ef730d919807
Cadu <cadumalmeida13@gmail.com>
2025-09-24 02:01:46 -0300

    usando orientação a objeto de vdd finalmente

--- mainPessoa.java
+++ mainPessoa.java
@@ -4,9 +4,17 @@ public class mainPessoa {
 
         System.out.println("Nome: " + p1.nome); //acessa la na classe o nome da pessoa com o operado "."
         System.out.println("Idade: " + p1.idade);
-        System.out.println("trabalho: " + p1.ocupacao);
-        p1.apresentar(); // utiliza o metodo (função de classe)
-        p1.apresentar2("cadu",18,"estudante");
+        System.out.println("trabalho: " + p1.ocupacao + "\n");
+        //System.out.println("trabalho: " + p1.cpf); → dá erro e avisa que cpf é privado na class e pessoa
+
+        p1.apresentar(); // utiliza o método (função de classe)
+        p1.apresentar2("cadu",18,"estudante \n");
+
+        p1.apresentar2("cadu", p1.getIdade(), "estudante"); //o get pega o valor da variavel la na classe
+
+        p1.setIdade(18); //muda o valor da valiavel lá na classe pessoa
+        p1.apresentar(); //olá, meu nome é Belialuim tenho 18 e sou ator , (antes a idade era 22)
+
 
 
     }
--- pessoa.java
+++ pessoa.java
@@ -3,9 +3,15 @@ public class pessoa {
     //declara os atributos (variaveis com suas "características")
     public String nome = ("Belialuim");
     public Integer idade = 22;
-    public String ocupacao = ("ator");
     //qualquer um pode ver o acesso a partir do codigo main por ser "public"
 
+    protected String ocupacao = ("ator");
+    // o protected so deixa disponivel um atributo na pasta que a classe dele existe
+
+    private String cpf = ("232.940.120-00");
+    //o private pode ser um atributo ou metodo so disponivel na classe que pertence,se tentar acessa na main da erro
+
+    //declarando os metodos, (funçoes em classes)
     void apresentar(){
         System.out.println("olá, meu nome é " + nome + " tenho " + idade + " e sou " + ocupacao);
     }
@@ -14,4 +20,11 @@ public class pessoa {
         System.out.println("olá, meu nome é " + nome + " tenho " + idade + " e sou " + ocupacao);
     }
 
+    public void setIdade(Integer idade) { //cria uma função para mudar a variavel na main
+        this.idade = idade;
+    }
+
+    public Integer getIdade() {
+        return idade;
+    }
 }
