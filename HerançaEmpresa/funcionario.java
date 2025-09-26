0c92ed21c0dcf9da85824124e1f63f808fd5a3e6
Cadu <cadumalmeida13@gmail.com>
2025-09-26 12:55:41 -0300

    funcionario update

--- HerançaEmpresa/funcionario.java
+++ HerançaEmpresa/funcionario.java
@@ -3,8 +3,8 @@ package HerançaEmpresa;
 public class funcionario extends gerente{ // herda os atributos publicos da classe gerente
    public Integer nivelDeAcesso = 1;
    public Float salario = 1200f;
-    public void SetIdade(Integer idade){
-       this.idade = idade; //herdou de gerente
+   public void SetIdade(Integer idade){
+        this.idade = idade; //herdou de gerente
    }
    public void SetSalario(Float salario){
        this.salario = salario;
