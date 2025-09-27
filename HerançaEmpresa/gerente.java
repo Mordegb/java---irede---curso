28f6fd3867abf241b7cd18baa426a49f3e1861f3
Cadu <cadumalmeida13@gmail.com>
2025-09-26 23:34:52 -0300

    tipo final em java

--- HerançaEmpresa/gerente.java
+++ HerançaEmpresa/gerente.java
@@ -6,7 +6,7 @@ public class gerente {
     public Integer idade;
     protected Float salario = 2500f;
     public Integer nivelDeAcesso = 2;
-
+    private final Double comissao = 25.000; // variavel imutalve, por ter o "final"
     public void Setsalario(Float salario){
         this.salario = salario;
     }
